76cc255e316a7b9c8f937fe17a174ca044c2c274
dwatanabe <watanabe@noova.jp>
2024-10-18 13:37:52 +0900

    build

--- lib/esm/types/types/tx.d.ts
+++ lib/esm/types/types/tx.d.ts
@@ -13,6 +13,7 @@ export interface CommonParams {
     tx: Transaction;
     coinIn?: TransactionObjectArgument;
     useCoinInDirectly?: boolean;
+    forceMinOutToZero?: boolean;
   };
 }
 export interface BuildTxParams extends CommonParams {
